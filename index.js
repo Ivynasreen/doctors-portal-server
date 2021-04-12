710b667b9081812b84631f10ef70bd31bddee623
Ivy nasreen <ivynasrn@gmail.com>
2021-04-13 00:19:49 +0600

    done

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
- 
-app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const uri = "mongodb+srv://doctorSabrina:sabrina1234@cluster0.9d6zf.mongodb.net/doctorsPortal?retryWrites=true&w=majority";
 
 const app = express()
 
+app.use(bodyParser.json());
+app.use(cors());
 
-const port = 5200;
-app.get('/', function (req, res) {
-  res.send('Hello World')
+
+const port = 5500;
+
+app.get('/', (req, res) => {
+    res.send("hello from db it's working working")
 })
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const collection = client.db("doctorsPortal").collection("appointment");
